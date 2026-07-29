a01148547d8f64b3342ea6363c304a2284364460
cho-heeyeon <a73037257@gmail.com>
2026-07-30 05:52:33 +0900

    Day001

---
+++ main.py
@@ -0,0 +1,18 @@
+# Day001
+# 숫자 분석 프로그램
+
+numbers = [15, 8, 23, 42, 7, 31]
+
+print("원본 데이터 :", numbers)
+
+print("합계 :", sum(numbers))
+print("평균 :", sum(numbers)/len(numbers)) 
+print("최대값 :", max(numbers))
+print("최소값 :", min(numbers))
+
+numbers.sort()
+
+print("오름차순 :", numbers)
+
+
+
